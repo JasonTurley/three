fb6029271b01811c0537ceaeb6e0389e40eabc9b
JasonTurley <jasonturley0@gmail.com>
2019-09-27 21:08:24 -0500

    add more three functions

--- three.py
+++ three.py
@@ -5,14 +5,38 @@ def three():
     return 3
 
 def dollars():
-    print('$3')
+    return '$3.00'
+
+def cents():
+    return '$0.03'
+
+def factorial():
+    """
+    Returns 3*2*1. Optimized at 300% computation time.
+    """
+    return 6
+
+def three_filter(old_list : list):
+    new_list = list(filter(lambda x : x == 3, old_list))
+    return new_list
 
 # Novelty
 def rule_of():
-    print ('Things that come in 3s are inherently more appealing.')
+    return 'Things that come in 3s are inherently more appealing.'
 
 def musketeers():
-    return ['A', 'B', 'C']
+    return ['Athos', 'Aramis', 'Porthos']
 
 def stooges():
     return ['Larry', 'Curly', 'Moe']
+
+# Food
+def leches():
+    return ['Condensed', 'Evaporated', 'Heavy cream']
+
+def peas():
+    return 'As close as three peas in a pod'
+
+# Mythology
+def cerberus():
+    return 'The hound of Hades: A three-headed dog guarding the Underworld and preventing the dead from leaving.'

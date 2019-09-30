f83bf738f21967f881d2b624a37a62c23ae293d1
JasonTurley <jasonturley0@gmail.com>
2019-09-29 21:55:35 -0500

    add squared(), cubed(), and primary_colors()

--- three.py
+++ three.py
@@ -1,8 +1,29 @@
 #!/usr/bin/ python3
 
+##############################################################################
 # Numbers
+##############################################################################
+
 def three():
     return 3
+
+def squared():
+    return 9
+
+def cubed():
+    return 27
+
+def factorial():
+    # Returns 3*2*1. Optimized at 300% computation time.
+    return 6
+
+def three_filter(old_list : list):
+    new_list = list(filter(lambda x : x == 3, old_list))
+    return new_list
+
+##############################################################################
+# Currency
+##############################################################################
 
 def dollars():
     return '$3.00'
@@ -10,17 +31,10 @@ def dollars():
 def cents():
     return '$0.03'
 
-def factorial():
-    """
-    Returns 3*2*1. Optimized at 300% computation time.
-    """
-    return 6
+##############################################################################
+# Misc
+##############################################################################
 
-def three_filter(old_list : list):
-    new_list = list(filter(lambda x : x == 3, old_list))
-    return new_list
-
-# Novelty
 def rule_of():
     return 'Things that come in 3s are inherently more appealing.'
 
@@ -30,13 +44,22 @@ def musketeers():
 def stooges():
     return ['Larry', 'Curly', 'Moe']
 
+def primary_colors():
+    return ['Red', 'Yellow', 'Blue']
+
+##############################################################################
 # Food
+##############################################################################
+
 def leches():
     return ['Condensed', 'Evaporated', 'Heavy cream']
 
 def peas():
     return 'As close as three peas in a pod'
 
+##############################################################################
 # Mythology
+##############################################################################
+
 def cerberus():
     return 'The hound of Hades: A three-headed dog guarding the Underworld and preventing the dead from leaving.'

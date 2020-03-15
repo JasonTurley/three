04b3bb058be5031f128c8583f44a178331261888
JasonTurley <jasonturley0@gmail.com>
2020-03-14 20:07:37 -0500

    made pep8 compliant

--- three.py
+++ three.py
@@ -4,27 +4,34 @@
 # Numbers                                                                    #
 ##############################################################################
 
+
 def three():
     return 3
+
 
 def squared():
     return 9
 
+
 def cubed():
     return 27
+
 
 def dozen():
     return 36
 
+
 def binary():
     return bin(3)
+
 
 def factorial():
     # Returns 3*2*1. Optimized at 300% computation time.
     return 6
 
-def three_filter(old_list : list):
-    new_list = list(filter(lambda x : x == 3, old_list))
+
+def three_filter(old_list):
+    new_list = list(filter(lambda x: x == 3, old_list))
     return new_list
 
 
@@ -32,8 +39,10 @@ def three_filter(old_list : list):
 # Currency                                                                   #
 ##############################################################################
 
+
 def dollars():
     return '$3.00'
+
 
 def cents():
     return '$0.03'
@@ -43,17 +52,22 @@ def cents():
 # Misc                                                                       #
 ##############################################################################
 
+
 def rule_of():
     return 'Things that come in 3s are inherently more appealing.'
+
 
 def musketeers():
     return ['Athos', 'Aramis', 'Porthos']
 
+
 def stooges():
     return ['Larry', 'Curly', 'Moe']
 
+
 def primary_colors():
     return ['Red', 'Yellow', 'Blue']
+
 
 ##############################################################################
 # Food                                                                       #
@@ -61,6 +75,7 @@ def primary_colors():
 
 def leches():
     return ['Condensed', 'Evaporated', 'Heavy cream']
+
 
 def peas():
     return 'As close as three peas in a pod'
@@ -70,5 +85,7 @@ def peas():
 # Mythology                                                                  #
 ##############################################################################
 
+
 def cerberus():
-    return 'The hound of Hades: A three-headed dog guarding the Underworld and preventing the dead from leaving.'
+    return """The hound of Hades: A three-headed dog guarding the Underworld
+    and preventing the dead from leaving"""

b82e576b718c7386cc2f552963d3481c17caf1b8
000Nobody <65867770+000Nobody@users.noreply.github.com>
2020-10-02 21:39:13 -0400

    Update three.py

--- three/three.py
+++ three/three.py
@@ -33,7 +33,7 @@ def is_three(x):
 
 
 def filter(items):
-    return [item for item in items if is_three(item) == True]
+    return [item for item in items if is_three(item)]
 
 
 def map(items):
@@ -41,7 +41,29 @@ def map(items):
 
 
 def reduce(items):
-    return three()    
+    return three()
+
+
+# Language
+
+def letters():
+    return ['t', 'h', 'r', 'e', 'e']
+
+
+def spanish():
+    return 'tres'
+
+
+def german():
+    return 'drei'
+
+
+def french():
+    return 'drois'
+
+
+def italian():
+    return 'tre'
 
 
 # Currency
@@ -55,11 +77,17 @@ def cents():
 
 
 # Rule of Threes
+
 def rule_of():
     return 'Things that come in 3s are inherently more appealing.'
 
 
+def is_appealing(items):
+    return len(items) == 3
+
+
 # Novelty 
+
 def musketeers():
     return ['Athos', 'Aramis', 'Porthos']
 
@@ -77,6 +105,11 @@ def little_pigs():
     return ['Straw', 'Sticks', 'Bricks']
 
 
+def blind_mice():
+    """Returns the names of the three blind mice in shrek"""
+    return ['Forder', 'Gorder', 'Horder']
+
+
 # Food
 
 def leches():
@@ -85,3 +118,9 @@ def leches():
 
 def peas():
     return 'As close as three peas in a pod'
+
+
+# Misc
+
+def string():
+    return '3'
